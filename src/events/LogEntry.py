5caac2d0fb0e155a3b00c534d2d5f63502883864
Chris Sammis <csammis@gmail.com>
2014-07-09 16:56:54 -0500

    Added a LogEntry object

---
+++ src/events/LogEntry.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, UnicodeText, DateTime
+from sqlalchemy import func
+from events import Base
+
+class LogEntry(Base) :
+
+    __tablename__ = 'LogEntries'
+    id = Column(Integer, primary_key = True)
+    entry_time = Column(DateTime, default=func.now())
+    entry = Column(UnicodeText, nullable = True)
+
+    def __init__(self, entry, entry_time = None) :
+        self.entry = entry
+        self.entry_time = entry_time
