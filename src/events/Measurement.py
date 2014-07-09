aa934a179d7a2587b544f472ce15deb81897e49f
Chris Sammis <csammis@gmail.com>
2014-07-09 16:33:23 -0500

    I discovered SQLAlchemy exists so now there's that

--- src/events/Measurement.py
+++ src/events/Measurement.py
@@ -1,44 +1,20 @@
-import time
+from sqlalchemy import Column, Integer, Float, DateTime
+from sqlalchemy import func
+from events import Base
 
-class Measurement(object) :
+class Measurement(Base) :
+
+    __tablename__ = 'Measurements'
+    id = Column(Integer, primary_key = True)
+    measurement_time = Column(DateTime, default=func.now())
+    measurement_type = Column(Integer)
+    value = Column(Float, nullable = True)
 
     ' Measurement types '
     PHOSPHATE, MAGNESIUM, CALCIUM, KH, PH, SPECIFICGRAVITY, TEMPERATURE, NITRATE = range(0, 8)
 
-    def __init__(self) :
-        self._hasvalue = False
-        self._time = None
-        super(Measurement, self).__init__()
-
-    
-    @property
-    def value(self) :
-        return self._value if self._hasvalue else None
-
-    @value.setter
-    def value(self, typeValue) :
-        try :
-            measurementType, value = typeValue
-        except ValueError :
-            raise ValueError("Pass both measurement type and value")
-
-        self._type = measurementType 
-        self._value = value
-        self._hasvalue = True
-        self.time = time.time()
-
-    @property
-    def has_value(self) :
-        return self._hasvalue
-
-    @property
-    def measurement_type(self) :
-        return self._type if self._hasvalue else None
-
-    @property
-    def time(self) :
-        return self._time
-
-    @time.setter
-    def time(self, time) :
-        self._time = time
+    def __init__(self, measurement_type, measurement_time = None, value = None) :
+        self.measurement_type = measurement_type
+        self.measurement_time = measurement_time
+        self.value = value
+ 
