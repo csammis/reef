3d6006b08f4fe744069a70cc581f3d7101e69196
Chris Sammis <csammis@gmail.com>
2014-07-11 16:23:45 -0500

    Changed to using flask-restful for the API pieces

--- src/app.py
+++ src/app.py
@@ -2,53 +2,59 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import abort
+from flask.ext import restful
 from datetime import datetime
-
 import events
 
 app = Flask(__name__)
+api = restful.Api(app)
 event_manager = events.EventManager('events/events.db')
 
 @app.route('/')
 def index():
     return 'an index page here'
 
-@app.route('/measurements/')
-def measurements():
+@app.route('/parameters/')
+def parameters():
     return 'this is where all the pretty graphs might go'
 
 def timerange_from_request():
+    trange = None
     try:
-        start = int(request.args['start'])
+        if 'start' in request.args.keys():
+            start = int(request.args['start'])
+        else:
+            start = 0
         trange = (datetime.fromtimestamp(start),)
-    except (KeyError, ValueError):
+    except ValueError:
         abort(400)
 
-    end = request.args.get('end', '')
-    if end != '':
-        try:
-            trange += (datetime.fromtimestamp(int(end)),)
-        except ValueError:
-            abort(400)
+    try:
+        if 'end' in request.args.keys():
+            end = int(request.args['end'])
+            trange += (datetime.fromtimestamp(end),)
+        else:
+            trange += (datetime.utcnow(),)
+    except ValueError:
+        abort(400)
 
     return trange
 
+def parameters_from_request():
+    parameters = None
+    if 'parameters' in request.args.keys():
+        parameters = request.args['parameters'].split(',')
+    return parameters
 
-@app.route('/measurements/<time_range>')
-def get_measurements(time_range):
-    if time_range == 'all':
-        events = event_manager.get_measurements()
-    elif time_range == 'range':
+# Define a resource endpoint for measurements
+class Measurements(restful.Resource):
+    def get(self):
         trange = timerange_from_request()
-        events = event_manager.get_measurements(trange)
-    else:
-        abort(400)
+        parameters = parameters_from_request()
+        return jsonify(time_start = trange[0], time_end = trange[1], \
+                events = event_manager.get_measurements(parameters = parameters, timerange = trange))
 
-    return jsonify(range = time_range, events = events)
-
-@app.route('/measurements/add', methods=['POST'])
-def add_measurement():
-    pass
+api.add_resource(Measurements, '/measurements')
 
 if __name__ == '__main__':
     app.run(debug=True)
