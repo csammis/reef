fa62a89f3dfdf6d0f669a79dc840b02ef97bd3c7
Chris Sammis <csammis@gmail.com>
2014-07-15 16:34:29 -0500

    Incorporating per-measurement chart configurations - axis labels, ideal regions, chart labels

--- src/static/parameters.js
+++ src/static/parameters.js
@@ -2,12 +2,34 @@
     var WIDTH = 1000;
     var HEIGHT = 400;
     var HORIZONTAL_PADDING = 35;
+    var MARGIN = {
+            LEFT: 20,
+            RIGHT: 0,
+            TOP: 0,
+            BOTTOM: 20 };
     var VERTICAL_PADDING = 20;
 
     var svgs = {};
+    var configs = {};
     var timeFormat = d3.time.format('%Y-%m-%dT%H:%M:%S');
 
-    function sendRequest() {
+    function sendConfigRequest() {
+        $.ajax({
+            url: '/configs/measurements',
+            type: 'GET',
+            dataType: 'json',
+            success: function(json) {
+                stashConfigs(json);
+            }
+        });
+    };
+
+    function stashConfigs(json) {
+        configs = json.configs;
+        sendDataRequest();
+    };
+
+    function sendDataRequest() {
         $.ajax({
             url: '/measurements/',
             type: 'GET',
@@ -20,10 +42,17 @@
 
     function getSvgForParameter(parameter) {
         if (!svgs.hasOwnProperty(parameter)) {
-            $('#graphs').append('<h3>' + parameter + '</h3>').append('<div id="graph' + parameter + '"></div>').addClass('graphdiv');
+            var label = parameter;
+            if (configs.hasOwnProperty(parameter)) {
+                label = configs[parameter].measurement_label;
+            }
+
+            $('#graphs').append('<h3>' + label + '</h3>').append('<div id="graph' + parameter + '"></div>').addClass('graphdiv');
             svgs[parameter] = d3.select('#graph' + parameter).append('svg:svg')
-                .attr('width', WIDTH)
-                .attr('height', HEIGHT);
+                .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+                .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+                .append('g')
+                .attr('transform', 'translate(' + MARGIN.LEFT + ',' + MARGIN.TOP + ')');
         }
 
         return svgs[parameter];
@@ -56,13 +85,14 @@
             
             var yScale = d3.scale.linear()
                 .range([HEIGHT - VERTICAL_PADDING, VERTICAL_PADDING])
-                .domain(d3.extent(ds, function(d) { return d.value; })).nice(1.2);
+                .domain(d3.extent(ds, function(d) { return d.value; })).nice(2);
 
             var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
             var yAxis = d3.svg.axis().scale(yScale).orient('left');
 
             var s = getSvgForParameter(mt);
 
+            // Axes and common labeling
             s.append('g')
                 .attr('class', 'xAxis')
                 .attr('transform', 'translate(0,' + (HEIGHT - VERTICAL_PADDING) + ')')
@@ -72,6 +102,36 @@
                 .attr('class', 'yAxis')
                 .attr('transform', 'translate(' + HORIZONTAL_PADDING + ',0)')
                 .call(yAxis);
+
+            s.append('text')
+                .attr('x', (WIDTH + MARGIN.LEFT) / 2)
+                .attr('y', HEIGHT + MARGIN.BOTTOM)
+                .style('text-anchor', 'middle')
+                .style('font-family', 'sans-serif')
+                .text('Measurement date');
+
+            // Draw any customizations that are configured for this measurement
+            if (configs.hasOwnProperty(mt)) {
+                var v0 = Math.min(configs[mt].range[0], configs[mt].range[1]);
+                var v1 = Math.max(configs[mt].range[0], configs[mt].range[1]);
+
+                s.append('rect')
+                    .attr('x', HORIZONTAL_PADDING)
+                    .attr('y', yScale(v1))
+                    .attr('width', WIDTH - (HORIZONTAL_PADDING * 2))
+                    .attr('height', yScale(v0) - yScale(v1))
+                    .attr('fill', 'lightgreen')
+                    .attr('stroke', 'darkgreen');
+
+                s.append('text')
+                    .attr('transform', 'rotate(-90)')
+                    .attr('y', 0 - MARGIN.LEFT)
+                    .attr('x', 0 - (HEIGHT / 2))
+                    .attr('dy', '1em')
+                    .style('text-anchor', 'middle')
+                    .style('font-family', 'sans-serif')
+                    .text(configs[mt].value_label);
+            }
 
             // Draw connecting lines between measurements of each type
             var lineFunction = d3.svg.line()
@@ -98,5 +158,5 @@
         // Bind up html controls
     });
 
-    window.onload = sendRequest();
+    window.onload = sendConfigRequest();
 })();
