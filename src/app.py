98d0750eee261a43cae3afb9a28d1c3c9c65a370
Chris Sammis <csammis@gmail.com>
2014-07-15 16:50:05 -0500

    Adding more per-measurement configs, support for partial configs

--- src/app.py
+++ src/app.py
@@ -107,6 +107,12 @@ class Config(restful.Resource):
         if config_type == 'measurements':
             configs = [];
             configs.append(events.MeasurementConfig(events.MeasurementType.Temperature, 'Temp', [78, 82], 'Degrees F'))
+            configs.append(events.MeasurementConfig(events.MeasurementType.KH, 'Alkalinity', [125, 200], 'ppm CaCO3'))
+            configs.append(events.MeasurementConfig(events.MeasurementType.PH, 'pH', [8.1, 8.4], ''))
+            configs.append(events.MeasurementConfig(events.MeasurementType.Phosphate, value_label='parts per million'))
+            configs.append(events.MeasurementConfig(events.MeasurementType.Calcium, value_range=[420, 500], value_label='parts per million'))
+            configs.append(events.MeasurementConfig(events.MeasurementType.SpecificGravity, measurement_label='Specific gravity'))
+            configs.append(events.MeasurementConfig(events.MeasurementType.Magnesium, value_range=[1250, 1350], value_label='parts per million'))
 
             configDict = {c.measurement_type.name: {'range': c.value_range, 'measurement_label': c.measurement_label, 'value_label': c.value_label } \
                     for c in configs}
--- src/events/MeasurementConfig.py
+++ src/events/MeasurementConfig.py
@@ -1,7 +1,7 @@
 
 class MeasurementConfig(object) :
 
-    def __init__(self, measurement_type, measurement_label, value_range, value_label) :
+    def __init__(self, measurement_type, measurement_label = None, value_range = None, value_label = None) :
         self.measurement_type = measurement_type
         self.value_range = value_range
         self.measurement_label = measurement_label
