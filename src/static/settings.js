6a02d607e0d7f42864b5a02c62032af0907cf1e5
Chris Sammis <csammis@gmail.com>
2014-07-29 15:27:46 -0500

    UI support for inline editing of a measurement type

--- src/static/settings.js
+++ src/static/settings.js
@@ -24,18 +24,18 @@
                     function() { $(this).removeClass('entry-hover'); $('.measurement-type-' + config.id).hide(); }
                 );
 
-        $('<td>').html(config.label).appendTo($row);
-        $('<td>').html(config.units).appendTo($row);
+        $('<td>').addClass(config.id + '-label').html(config.label).appendTo($row);
+        $('<td>').addClass(config.id + '-units').html(config.units).appendTo($row);
 
         var range_html = '';
         if (config.acceptable_range !== null && config.acceptable_range !== undefined) {
             range_html = Math.min(config.acceptable_range[0], config.acceptable_range[1]) + ' - ' + Math.max(config.acceptable_range[0], config.acceptable_range[1]);
         }
-        $('<td>').html(range_html).appendTo($row);
+        $('<td>').addClass(config.id + '-range').html(range_html).appendTo($row);
 
         var $control = $('<span>').addClass('measurement-type-' + config.id).hide();
         $('<a>').attr('href', '#').html('<img src="/static/images/edit.svg" alt="Edit this measurement type" class="icon" />').click(function() {
-            
+            onEditMeasurementType(config.id);
         }).appendTo($control);
         $('<a>').attr('href', '#').html('<img src="/static/images/delete.svg" alt="Delete this measurement type" class="icon" />').click(function() {
             $.ajax({
@@ -47,31 +47,82 @@
                     complete: function() { $(this).remove(); }
                 });
             })
-            .fail(function(data) { alert(data.message); });
+            .fail(function(data) { alert(data.responseJSON.message); });
         }).appendTo($control);
 
-        $('<td>').append($control).appendTo($row);
+        $('<td>').addClass(config.id + '-control').append($control).appendTo($row);
 
         return $row;
     };
 
-    function addNewMeasurement() {
-        var label = $('#new_measurement_name').val();
-        var units = $('#new_measurement_units').val();
-        var range = $('#new_measurement_range').val();
-
+    function createJsonForUpdate(labelSelector, unitsSelector, rangeSelector) {
+        var range = $(rangeSelector).val();
         var ranges = range.split('-');
-        for (var i = 0; i < ranges.length; i++) {
+        for (i = 0; i < ranges.length; i++) {
             ranges[i] = ranges[i].trim();
         }
-
+        
         var config = {
-            'label': label,
-            'units': units
+            'label': $(labelSelector).val(),
+            'units': $(unitsSelector).val(),
+            'rangeHtml': range
         };
+
         if (range.length != 0) {
             config['acceptable_range'] = ranges;
         }
+
+        return config;
+    }
+
+    function onEditMeasurementType(id) {
+        var originals = {
+            labelElement: $('.' + id + '-label'),
+            label: $('.' + id + '-label').html(),
+            unitsElement: $('.' + id + '-units'),
+            units: $('.' + id + '-units').html(),
+            rangeElement: $('.' + id + '-range'),
+            range: $('.' + id + '-range').html(),
+            controlElement: $('.' + id + '-control'),
+            controlSpan: $('.measurement-type-' + id)
+        };
+
+        var finished = function() {
+            originals.labelElement.empty().html(originals.label);
+            originals.unitsElement.empty().html(originals.units)
+            originals.rangeElement.empty().html(originals.range);
+            originals.controlElement.empty().append(originals.controlSpan);
+        };
+
+        originals.labelElement.empty()
+            .append('<input type="text" value="' + originals.label + '" class="inline-edit" id="inline-edit-label-' + id + '" />');
+        originals.unitsElement.empty()
+            .append('<input type="text" value="' + originals.units + '" class="inline-edit" id="inline-edit-units-' + id + '" />');
+        originals.rangeElement.empty()
+            .append('<input type="text" value="' + originals.range + '" class="inline-edit" id="inline-edit-range-' + id + '" />');
+        originals.controlSpan.detach();
+
+        $('<a>').attr('href', '#').html('<img src="/static/images/submit-entry.svg" alt="Save changes" class="icon" />').click(function() {
+            var newConfig = createJsonForUpdate('#inline-edit-label-' + id, '#inline-edit-units-' + id, '#inline-edit-range-' + id);
+            $.ajax({
+                url: '/configs/measurements/' + id,
+                type: 'PUT',
+                dataType: 'json',
+                data: newConfig })
+            .done(function() {
+                originals.label = newConfig.label;
+                originals.units = newConfig.units;
+                originals.range = newConfig.rangeHtml;
+                finished();
+            })
+            .fail(function(data) { alert('Unable to update measurement type: ' + data.responseJSON.message); });
+
+        }).appendTo(originals.controlElement);
+        $('<a>').attr('href', '#').html('<img src="/static/images/cancel.svg" alt="Cancel edit" class="icon" />').click(finished).appendTo(originals.controlElement);
+    }
+
+    function addNewMeasurement() {
+        var config = createJsonForUpdate('#new_measurement_name', '#new_measurement_units', '#new_measurement_range');
 
         $.ajax({
             url: '/configs/measurements',
@@ -87,7 +138,9 @@
             $('#new_measurement_units').val('');
             $('#new_measurement_range').val('');
         })
-        .fail(function(data) { alert(data); });
+        .fail(function(data) {
+            alert(data.responseJSON.message);
+        });
         return false;
     }
 
