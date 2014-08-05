692a97a872e45e96613aaccf578927f696256683
Chris Sammis <csammis@gmail.com>
2014-08-04 22:26:48 -0500

    Preparing for plotting multiple Y-axis sets on a single chart

--- src/static/parameters.js
+++ src/static/parameters.js
@@ -64,7 +64,7 @@
                 .attr('transform', 'translate(0,' + (HEIGHT - VERTICAL_PADDING) + ')');
 
             svgs[mt].append('g')
-                .attr('class', 'yAxis')
+                .attr('class', 'yAxis0')
                 .attr('transform', 'translate(' + HORIZONTAL_PADDING + ',0)');
 
             svgs[mt].append('text')
@@ -90,41 +90,52 @@
 
                 svgs[mt].append('rect')
                     .attr('x', HORIZONTAL_PADDING)
-                    .attr('y', ds.yScale(v1))
+                    .attr('y', ds.yScale[0](v1))
                     .attr('width', WIDTH - (HORIZONTAL_PADDING * 2))
-                    .attr('height', ds.yScale(v0) - ds.yScale(v1))
+                    .attr('height', ds.yScale[0](v0) - ds.yScale[0](v1))
                     .attr('class', 'acceptable_range');
             }
 
             // Point connecting path element
             svgs[mt].append('path')
                 .attr('stroke', 'black')
-                .attr('class', 'line');
+                .attr('class', 'line y0');
 
             buildControlsForMeasurementType(mt);
         }
 
         return svgs[mt];
     }
+
+    function initializeNewDataset(yAxes) {
+        var dataset = {
+            data: new Array(),
+            xScale: d3.time.scale().range([HORIZONTAL_PADDING, WIDTH - HORIZONTAL_PADDING]),
+            yScale: new Array()
+        };
+
+        if (yAxes === undefined) {
+            yAxes = 1;
+        }
+
+        for (var i = 0; i < yAxes; i++) {
+            dataset.yScale.push(d3.scale.linear().range([HEIGHT - VERTICAL_PADDING, VERTICAL_PADDING]));
+        }
+
+        return dataset;
+    };
     
     function renderJson(json) {
         var full_dataset = json.measurements;
-
-        function initializeNewDataset() {
-            return {
-                data: new Array(),
-                xScale: d3.time.scale().range([HORIZONTAL_PADDING, WIDTH - HORIZONTAL_PADDING]),
-                yScale: d3.scale.linear().range([HEIGHT - VERTICAL_PADDING, VERTICAL_PADDING])
-            };
-        }
 
         // Split up the full dataset into measurement-specific slices
         for (var i = 0; i < full_dataset.length; i++) {
             var d = full_dataset[i];
             if (dataSplits[d.measurement_type_id] === undefined) {
                 dataSplits[d.measurement_type_id] = initializeNewDataset();
+                dataSplits[d.measurement_type_id].data.push(new Array());
             }
-            dataSplits[d.measurement_type_id].data.push(full_dataset[i]);
+            dataSplits[d.measurement_type_id].data[0].push(full_dataset[i]);
         }
         
         // Generate a graph and controls for each measurement type
@@ -213,46 +224,46 @@
         function yPos(d) { return d.value; };
 
         var ds = dataSplits[mt];
-        ds.xScale.domain(d3.extent(ds.data, xPos));
-        ds.yScale.domain(d3.extent(ds.data, yPos)).nice(2);
+        ds.xScale.domain(d3.extent(ds.data[0], xPos));
+        ds.yScale[0].domain(d3.extent(ds.data[0], yPos)).nice(2);
 
         var s = getSvgForParameter(mt);
 
         // Draw connecting lines between measurements of each type
         var lineFunction = d3.svg.line()
             .x(function(d) { return ds.xScale(timeFormat.parse(d.measurement_time)); })
-            .y(function(d) { return ds.yScale(d.value); })
+            .y(function(d) { return ds.yScale[0](d.value); })
             .interpolate('linear');
         
-        var points = s.selectAll('circle').data(ds.data);
-        var line = s.select('path.line').data(ds.data);
+        var points = s.selectAll('circle.y0').data(ds.data[0]);
+        var line = s.select('path.line.y0').data(ds.data[0]);
         
         points.transition()
             .attr('cx', function(d) { return ds.xScale(xPos(d)); })
-            .attr('cy', function(d) { return ds.yScale(yPos(d)); });
+            .attr('cy', function(d) { return ds.yScale[0](yPos(d)); });
         line.transition()
-            .attr('d', lineFunction(ds.data));
+            .attr('d', lineFunction(ds.data[0]));
 
         if (configs[mt].acceptable_range) {
             var v0 = Math.min(configs[mt].acceptable_range[0], configs[mt].acceptable_range[1]);
             var v1 = Math.max(configs[mt].acceptable_range[0], configs[mt].acceptable_range[1]);
             s.select('.acceptable_range').transition()
-                .attr('y', ds.yScale(v1))
-                .attr('height', ds.yScale(v0) - ds.yScale(v1));
+                .attr('y', ds.yScale[0](v1))
+                .attr('height', ds.yScale[0](v0) - ds.yScale[0](v1));
         }
 
         points.enter()
            .append('circle')
-           .attr('class', function(d) { return 'measurement_' + d.measurement_type; })
+           .attr('class', 'y0')
            .attr('cx', function(d) { return ds.xScale(xPos(d)); })
-           .attr('cy', function(d) { return ds.yScale(yPos(d)); })
+           .attr('cy', function(d) { return ds.yScale[0](yPos(d)); })
            .attr('r', 4);
 
         // Draw the axes
         var xAxis = d3.svg.axis().scale(ds.xScale).orient('bottom');
         s.selectAll('.xAxis').call(xAxis);
-        var yAxis = d3.svg.axis().scale(ds.yScale).orient('left');
-        s.selectAll('.yAxis').call(yAxis);
+        var yAxis = d3.svg.axis().scale(ds.yScale[0]).orient('left');
+        s.selectAll('.yAxis0').call(yAxis);
     }
 
     window.onload = sendConfigRequest();
