5bba0fc949d58e754ca25fe888dffaca8064aa42
Chris Sammis <csammis@gmail.com>
2014-08-12 17:00:12 -0500

    UI for CRUD operations on a Tank

--- src/static/settings.js
+++ src/static/settings.js
@@ -1,28 +1,36 @@
 (function() {
 
-    function sendConfigRequest() {
-        $.ajax({
-            url: '/configs/measurements',
-            type: 'GET',
-            dataType: 'json'})
-        .done(function(json) { buildMeasurementTypes(json); })
+    function sendConfigRequests() {
+        $.ajax({ url: '/configs/measurements', type: 'GET', dataType: 'json'})
+        .done(function(json) {
+            var $grid = $('#measurements_grid');
+            $grid.children('tr.data').remove();
+            for (var i = 0; i < json.configs.length; i++) {
+                $('#measurements_grid').append(buildMeasurementType(json.configs[i]));
+            }
+        })
         .fail(function() { alert('Failed to get configured measurement types'); });
+        
+        $.ajax({ url: '/configs/tanks', type: 'GET', dataType: 'json'})
+        .done(function(json) {
+            var $grid = $('#tanks_grid');
+            $grid.children('tr.data').remove();
+            for (var i = 0; i < json.tanks.length; i++) {
+                $grid.append(buildTank(json.tanks[i]));
+            }
+        })
+        .fail(function() { alert('Failed to get configured tanks'); });
     };
 
-    function buildMeasurementTypes(json) {
-        $('#measurements_grid').children('tr.data').remove();
-        for (var i = 0; i < json.configs.length; i++) {
-            var config = json.configs[i];
-            $('#measurements_grid').append(buildMeasurementType(config));
-        }
-    };
+    function buildTank(config) {
+        var $row = $('<tr>').addClass('data').attr('id', 'tank-id-' + config.id).hoverize();
+        $('<td>').attr('id', 'tank-name-' + config.id).html(config.name).appendTo($row);
+        createEditDeleteControlsInRow(config.id, 'tank', $row, onEditTank, onDeleteTank);
+        return $row;
+    }
 
     function buildMeasurementType(config) {
-        var $row = $('<tr>').addClass('data').attr('id', 'measurement-type-id-' + config.id)
-            .hover(
-                    function() { $(this).addClass('entry-hover'); },
-                    function() { $(this).removeClass('entry-hover'); }
-                );
+        var $row = $('<tr>').addClass('data').attr('id', 'measurement-type-id-' + config.id).hoverize();
 
         $('<td>').attr('id', 'label-' + config.id).html(config.label).appendTo($row);
         $('<td>').attr('id', 'units-' + config.id).html(config.units).appendTo($row);
@@ -32,35 +40,7 @@
             range_html = Math.min(config.acceptable_range[0], config.acceptable_range[1]) + ' - ' + Math.max(config.acceptable_range[0], config.acceptable_range[1]);
         }
         $('<td>').attr('id', 'range-' + config.id).html(range_html).appendTo($row);
-
-        var $control = $('<span>').attr('id', 'measurement-type-' + config.id);
-        $('<button>').append(
-                $('<img>').addClass('icon').attr('src','/static/images/edit.svg'))
-            .button()
-            .addClass('inline-button')
-            .click(function() { onEditMeasurementType(config.id); })
-            .appendTo($control);
-
-        $('<button>').append(
-                $('<img>').addClass('icon').attr('src', '/static/images/delete.svg'))
-            .button()
-            .addClass('inline-button')
-            .click(function() {
-                $.ajax({
-                    url: '/configs/measurements/' + config.id,
-                    type: 'DELETE',
-                    dataType: 'json'})
-                .done(function(json) {
-                    $row.fadeOut({
-                        complete: function() { $(this).remove(); }
-                    });
-                })
-                .fail(function(data) { alert(data.responseJSON.message); });
-            })
-            .appendTo($control);
-
-        $('<td>').attr('id', 'control-' + config.id).append($control).appendTo($row);
-
+        createEditDeleteControlsInRow(config.id, 'measurement-type', $row, onEditMeasurementType, onDeleteMeasurementType);
         return $row;
     };
 
@@ -84,12 +64,59 @@
         return config;
     }
 
-    function onEditMeasurementType(id) {
+    function onDeleteMeasurementType() {
+        var id = $(this).attr('data');
+        $.ajax({ url: '/configs/measurements/' + id, type: 'DELETE', dataType: 'json'})
+        .done(function(json) { $('#measurement-type-id-' + id).fadeRemove(); })
+        .fail(function(data) { alert(data.responseJSON.message); });
+    }
+
+    function onDeleteTank() {
+        var id = $(this).attr('data');
+        $.ajax({ url: '/configs/tanks/' + id, type: 'DELETE', dataType: 'json'})
+        .done(function(json) { $('#tank-id-' + id).fadeRemove(); })
+        .fail(function(data) { alert(data.responseJSON.message); });
+    }
+
+    function onEditTank() {
+        var id = $(this).attr('data');
+
+        var originals = {
+            nameElement: $('#tank-name-' + id),
+            controlElement: $('#control-tank-' + id),
+            controlSpan: $('#tank-' + id)
+        };
+        originals['name'] = originals.nameElement.html();
+
+        var finished = function() {
+            originals.nameElement.empty().html(originals.name);
+            originals.controlElement.empty().append(originals.controlSpan);
+        };
+
+        var submitEdit = function() {
+            var config = {'name': $('#inline-edit-tank-name-' + id).val()};
+            $.ajax({ url: '/configs/tanks/' + id, type: 'PUT', dataType: 'json', data: config})
+            .done(function() {
+                originals.name = config.name;
+                finished();
+            })
+            .fail(function(data) { alert('Unable to update tank: ' + data.responseJSON.message); });
+        }
+
+        inlineEditize(originals.nameElement, 'inline-edit-tank-name-' + id);
+        createSaveCancelControls(originals, submitEdit, finished);
+        bindInputsToKeyHandler('#tank-id-' + id, submitEdit, finished);
+        $('#inline-edit-tank-name-' + id).focus().select();
+    }
+
+    function onEditMeasurementType() {
+        var id = $(this).attr('data');
+
         var originals = {
             labelElement: $('#label-' + id),
             unitsElement: $('#units-' + id),
             rangeElement: $('#range-' + id),
-            controlElement: $('#control-' + id),
+            controlElement: $('#control-measurement-type-' + id),
             controlSpan: $('#measurement-type-' + id)
         };
         originals['label'] = originals.labelElement.html();
@@ -105,11 +132,7 @@
 
         var submitEdit = function() {
             var newConfig = createJsonForUpdate('#inline-edit-label-' + id, '#inline-edit-units-' + id, '#inline-edit-range-' + id);
-            $.ajax({
-                url: '/configs/measurements/' + id,
-                type: 'PUT',
-                dataType: 'json',
-                data: newConfig })
+            $.ajax({ url: '/configs/measurements/' + id, type: 'PUT', dataType: 'json', data: newConfig })
             .done(function() {
                 originals.label = newConfig.label;
                 originals.units = newConfig.units;
@@ -119,52 +142,18 @@
             .fail(function(data) { alert('Unable to update measurement type: ' + data.responseJSON.message); });
         };
 
-        originals.labelElement.empty()
-            .append(
-                    $('<input>').attr('type', 'text')
-                        .attr('id', 'inline-edit-label-' + id)
-                        .addClass('inline-edit')
-                        .val(originals.label));
-        originals.unitsElement.empty()
-            .append(
-                    $('<input>').attr('type', 'text')
-                        .attr('id', 'inline-edit-units-' + id)
-                        .addClass('inline-edit')
-                        .val(originals.units));
-        originals.rangeElement.empty()
-            .append(
-                    $('<input>').attr('type', 'text')
-                        .attr('id', 'inline-edit-range-' + id)
-                        .addClass('inline-edit')
-                        .val(originals.range));
-
+        inlineEditize(originals.labelElement, 'inline-edit-label-' + id);
+        inlineEditize(originals.unitsElement, 'inline-edit-units-' + id);
+        inlineEditize(originals.rangeElement, 'inline-edit-range-' + id);
+        createSaveCancelControls(originals, submitEdit, finished);
         bindInputsToKeyHandler('#measurement-type-id-' + id, submitEdit, finished);
         $('#inline-edit-label-' + id).focus().select();
-
-        originals.controlSpan.detach();
-        $('<button>').append(
-                $('<img>').addClass('icon').attr('src', '/static/images/submit-entry.svg'))
-            .button()
-            .addClass('inline-button')
-            .click(submitEdit)
-            .appendTo(originals.controlElement);
-
-        $('<button>').append(
-                $('<img>').addClass('icon').attr('src', '/static/images/cancel.svg'))
-            .button()
-            .addClass('inline-button')
-            .click(finished)
-            .appendTo(originals.controlElement);
     }
 
     function addNewMeasurement() {
         var config = createJsonForUpdate('#new_measurement_name', '#new_measurement_units', '#new_measurement_range');
 
-        $.ajax({
-            url: '/configs/measurements',
-            type: 'POST',
-            dataType: 'json',
-            data: config})
+        $.ajax({ url: '/configs/measurements', type: 'POST', dataType: 'json', data: config})
         .done(function(json) {
             buildMeasurementType(json.measurement_type)
                 .hide()
@@ -174,16 +163,92 @@
             $('#new_measurement_units').val('');
             $('#new_measurement_range').val('');
         })
-        .fail(function(data) {
-            alert(data.responseJSON.message);
-        });
+        .fail(function(data) { alert(data.responseJSON.message); });
         return false;
     }
 
+    function addNewTank() {
+        var config = {'name': $('#new_tank_name').val() };
+
+        $.ajax({ url: '/configs/tanks', type: 'POST', dataType: 'json', data: config})
+        .done(function(json) {
+            buildTank(json.tank)
+                .hide()
+                .appendTo($('#tanks_grid'))
+                .fadeIn();
+            $('#new_tank_name').val('').focus();
+        })
+        .fail(function(data) { alert(data.responseJSON.message); });
+        return false;
+    };
+
     window.onload = function() {
         $('#submit_new_measurement').button().click(addNewMeasurement);
+        $('#submit_new_tank').button().click(addNewTank);
+        bindInputsToKeyHandler('.tank_new_entry', addNewTank);
         bindInputsToKeyHandler('.measurement_new_entry', addNewMeasurement);
-        sendConfigRequest();
+
+        $.fn.hoverize = function() {
+            return this.each(function() {
+                $(this).hover(function() { $(this).addClass('entry-hover'); }, function() { $(this).removeClass('entry-hover'); });
+            });
+        };
+
+        $.fn.fadeRemove = function() {
+            return this.each(function() {
+                $(this).fadeOut({ 'complete': function() { $(this).remove(); } });
+            });
+        };
+
+        sendConfigRequests();
+    };
+
+    // DOM building helper functions
+    
+    function createEditDeleteControlsInRow(id, forSetting, $tr, editFunction, deleteFunction) {
+        var $control = $('<span>').attr('id', forSetting + '-' + id);
+
+        $('<button>').append(
+                $('<img>').addClass('icon').attr('src','/static/images/edit.svg'))
+            .button()
+            .addClass('inline-button')
+            .attr('data', id)
+            .click(editFunction)
+            .appendTo($control);
+
+        $('<button>').append(
+                $('<img>').addClass('icon').attr('src', '/static/images/delete.svg'))
+            .button()
+            .addClass('inline-button')
+            .attr('data', id)
+            .click(deleteFunction)
+            .appendTo($control);
+
+        $('<td>').attr('id', 'control-' + forSetting + '-' + id)
+            .append($control)
+            .appendTo($tr);
+    };
+
+    function createSaveCancelControls(originals, saveFunction, cancelFunction) {
+        originals.controlSpan.detach();
+        $('<button>').append(
+                $('<img>').addClass('icon').attr('src', '/static/images/submit-entry.svg'))
+            .button()
+            .addClass('inline-button')
+            .click(saveFunction)
+            .appendTo(originals.controlElement);
+
+        $('<button>').append(
+                $('<img>').addClass('icon').attr('src', '/static/images/cancel.svg'))
+            .button()
+            .addClass('inline-button')
+            .click(cancelFunction)
+            .appendTo(originals.controlElement);
+    };
+
+    function inlineEditize($element, id) {
+        var value = $element.html();
+        $element.empty().append($('<input>').attr('type','text').attr('id',id).addClass('inline-edit').val(value));
     };
 
 })();
